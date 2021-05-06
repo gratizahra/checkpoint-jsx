97d61201f7399abe29034b19389b055f9f9389ac
gratizahra <82332990+gratizahra@users.noreply.github.com>
2021-05-06 22:31:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import './style.css';
 import imageinsrc from"./img.png"
@@ -6,18 +5,19 @@ import imageinsrc from"./img.png"
 
 function App() {
   return (
-    <div className="App">
+ <div className="App">
       <header className="App-header">
   
-  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
-    <h1 className='title red'>Your name here</h1>
+  <div>
+    <h1 className='title red'>Zahra Grati</h1>
+    <p className='parag'>Welcome to my web page</p>
     <br />
     <img src={imageinsrc} className="myimage" alt="imageinsrc" />
     <br />
     <img src={"/image.png"} className="imageinPublic" alt="imageinpublic"></img>
-  <video width={320} height={240} controls>
-    <source src="myVideo.mp4" type="video/mp4" />
-  </video>
+    <div className='vid'>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/7fPXI_MnBOY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
       </div>
       </header>
   </div>
